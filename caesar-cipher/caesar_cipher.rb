138da18ac0e82a10300029bd7eb74c5249ff2949
parentda <danielparent21@gmail.com>
2021-08-12 10:11:50 -0400

    Refactored caesar_cipher exercise

--- caesar-cipher/caesar_cipher.rb
+++ caesar-cipher/caesar_cipher.rb
@@ -3,25 +3,9 @@ def caesar_cipher(string, shift_factor = 0)
     .chars
     .map do |char|
       if char.match?(/[A-Za-z]/)
-        numeric_rep = char.ord
-        shift = shift_factor.remainder(26)
-        if numeric_rep >= 65 && numeric_rep <= 90
-          if numeric_rep + shift > 90
-            (65 + (numeric_rep + shift - 1) - 90).chr
-          elsif numeric_rep + shift < 65
-            (90 + (numeric_rep + shift + 1) - 65).chr
-          else
-            (numeric_rep + shift).chr
-          end
-        elsif numeric_rep >= 97 && numeric_rep <= 122
-          if numeric_rep + shift > 122
-            (97 + (numeric_rep + shift - 1) - 122).chr
-          elsif numeric_rep + shift < 97
-            (122 + (numeric_rep + shift + 1) - 97).chr
-          else
-            (numeric_rep + shift).chr
-          end
-        end
+        char_numeric = char.ord
+        base = char_numeric < 97 ? 65 : 97
+        ((((char_numeric - base) + shift_factor) % 26) + base).chr
       else
         char
       end
