f4d1aa2a9f6548a810709ab90c7f8098582ac5b1
parentda <danielparent21@gmail.com>
2021-08-12 13:37:47 -0400

    Refactored stock_picker exercise

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,18 +1,20 @@
 def stock_picker(stock_price_array)
-  buy_day = 0
-  sell_day = 0
+  min_price = { value: stock_price_array[0], index: 0 }
   max_profit = 0
+  days = [0, 0]
 
   stock_price_array.each_with_index do |stock_price, index|
-    sell_day_options = stock_price_array.slice(index..)
-    max_sell_price = sell_day_options.max
-    if max_sell_price - stock_price > max_profit
-      buy_day = index
-      sell_day = sell_day_options.index(max_sell_price) + index
-      max_profit = max_sell_price - stock_price
+    if stock_price < min_price[:value]
+      min_price[:value] = stock_price
+      min_price[:index] = index
+    elsif stock_price - min_price[:value] > max_profit
+      days = [min_price[:index], index]
+      max_profit = stock_price - min_price[:value]
     end
   end
-  [buy_day, sell_day]
+  days
 end
 
 p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
+p stock_picker([1, 3, 6, 9, 15, 8, 6, 2, 17])
+p stock_picker([17, 15, 13, 11, 9, 8, 6, 3, 1])
