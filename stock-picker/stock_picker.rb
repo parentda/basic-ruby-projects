b5069ef0254a586064ff58bc7fce4a6ec6da4297
parentda <danielparent21@gmail.com>
2021-08-12 12:45:31 -0400

    Conmpleted stock_picker exercise

--- stock-picker/stock_picker.rb
+++ stock-picker/stock_picker.rb
@@ -1,1 +1,18 @@
-def stock_picker(stock_price_array); end
+def stock_picker(stock_price_array)
+  buy_day = 0
+  sell_day = 0
+  max_profit = 0
+
+  stock_price_array.each_with_index do |stock_price, index|
+    sell_day_options = stock_price_array.slice(index..)
+    max_sell_price = sell_day_options.max
+    if max_sell_price - stock_price > max_profit
+      buy_day = index
+      sell_day = sell_day_options.index(max_sell_price) + index
+      max_profit = max_sell_price - stock_price
+    end
+  end
+  [buy_day, sell_day]
+end
+
+p stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
