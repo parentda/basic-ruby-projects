23feb41da09830a630a6f6e2a9322232fdf6512c
parentda <danielparent21@gmail.com>
2021-09-21 13:18:17 -0400

    added test case for shift factor of 0

--- caesar-cipher/spec/caesar_cipher_spec.rb
+++ caesar-cipher/spec/caesar_cipher_spec.rb
@@ -4,4 +4,8 @@ describe '#caesar_cipher' do
   it 'rejects non-string inputs' do
     expect(caesar_cipher(12_345, 5)).to eql('Invalid input format')
   end
+
+  it 'returns the same string when the shift factor is 0' do
+    expect(caesar_cipher('What a string!', 0)).to eql('What a string!')
+  end
 end
